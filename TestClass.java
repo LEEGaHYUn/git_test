812f86f6ca1f272ffa9ac18c2d18889242cba71e
LEEGaHYUn <dlrkgus06@gmail.com>
2020-01-14 16:09:56 +0900

    local change

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,6 @@ package test;
 public class TestClass {
     public static void main(String[] args){
         System.out.println("Hi1234567890!");
-        System.out.println("cooling!");
+        System.out.println("cooling~~~~~~~~~~~~~~~~~~!");
     }
 }
