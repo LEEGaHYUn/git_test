904690d6c0d2280fb4828568886fac95af9fbfc8
LEEGaHYUn <dlrkgus06@gmail.com>
2020-01-14 15:33:01 +0900

    merge test

--- TestClass.java
+++ TestClass.java
@@ -2,7 +2,6 @@ package test;
 public class TestClass {
     public static void main(String[] args){
         System.out.println("Hi1234567890!");
->>>>>>> new_branch
         System.out.println("cooling!");
     }
 }
