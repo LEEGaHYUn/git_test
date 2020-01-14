1d10df46b6c9256e3a570bfa1a859cf6a46635a3
LEEGaHYUn <dlrkgus06@gmail.com>
2020-01-14 15:29:47 +0900

    third commit in new_branch

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,6 @@
 package test;
 public class TestClass {
-    public static void main(Stringp[] args){
+    public static void main(String[] args){
         System.out.println("Hi1234567890!");
         System.out.println("cooling!");
     }
